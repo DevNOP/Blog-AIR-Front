93148fef492630cbcd359425a83ac30ca18a02dd
Igor2kLevi <99228640+Igor2kLevi@users.noreply.github.com>
2023-07-18 12:31:08 -0300

    Update teste
    Testando

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,6 +5,7 @@ function App() {
     <div className="h-screen flex flex-col justify-center items-center bg-black text-white">
       <h1 className="text-3xl font-bold underline mb-10">Hello world!</h1>
       <ButtonStart text={"Don't clicker"} />
+      <h2>Se clicar é gay.</h2>
     </div>
   )
 }
