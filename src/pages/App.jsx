711446a90889e078ce0da52a96d0bfa1d3be3d83
Robert Sampaio <robertfqsampaio@gmail.com>
2023-07-18 14:03:00 -0300

    text update

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -5,7 +5,7 @@ function App() {
     <div className="h-screen flex flex-col justify-center items-center bg-black text-white">
       <h1 className="text-3xl font-bold underline mb-10">Hello world! 🪐</h1>
       <h2 className="text-2xl font-bold mb-10">Welcome to our blog!</h2>
-      <ButtonStart text={"Don't clicker here"} />
+      <ButtonStart text={"Don't click here"} />
     </div>
   )
 }
