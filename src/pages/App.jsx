e070354aba0c5e14861dc088e321c9aea349847b
Robert Sampaio <robertfqsampaio@gmail.com>
2023-07-18 13:56:58 -0300

    add h2 and modified button text

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <div className="h-screen flex flex-col justify-center items-center bg-black text-white">
       <h1 className="text-3xl font-bold underline mb-10">Hello world! 🪐</h1>
-      <ButtonStart text={"Don't clicker"} />
+      <h2 className="text-2xl font-bold mb-10">Welcome to our blog!</h2>
+      <ButtonStart text={"Don't click here"} />
     </div>
   )
 }
