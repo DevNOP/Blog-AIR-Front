8b12cb34b109425d8dde61c43ebba795ba5309b1
AbnerSilvaBarbosa <abner.barbosa@sou.inteli.edu.br>
2023-07-24 14:13:13 -0300

    FEAT: add card Post

---
+++ src/components/cardPost.jsx
@@ -0,0 +1,34 @@
+export function CardPost({ image, title, qntComments, data, idCard, tags }) {
+  return (
+    <div
+      className="h-96 w-80 bg-gradient-to-b from-black to-blue-gradient-card rounded-lg transition ease-linear delay-75 cursor-pointer hover:scale-105"
+      onClick={() => {
+        console.log(idCard)
+      }}
+    >
+      <img className="w-96 h-44 object-cover rounded-lg" src={image} alt="" />
+
+      <div className="p-8">
+        <div className=" flex flex-wrap mb-3">
+          {tags.map((tag) => (
+            <p
+              key={tag}
+              className="text-white text-xs font-sans font-normal bg-blue-500 rounded-full px-2 mr-2 mb-2"
+            >
+              {tag}
+            </p>
+          ))}
+        </div>
+        <div className="line-clamp-2 mt-4">
+          <h1 className=" text-white text-xl font-sans font-normal ">
+            {title}
+          </h1>
+        </div>
+        <div className="flex text-white/40 justify-between text-sm mt-10">
+          <p>{qntComments} Comentários</p>
+          <p>{data}</p>
+        </div>
+      </div>
+    </div>
+  )
+}
